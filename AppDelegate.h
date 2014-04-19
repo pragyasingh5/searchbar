bb085680b9ee2f5e283487a2622ca28abc54684c
pragya5 <pragya.singh.mk@gmail.com>
2014-04-18 19:13:04 -0700

    Create AppDelegate.h

---
+++ AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  searchbar
+//
+//  Created by Pragya Singh on 4/18/14.
+//  Copyright (c) 2014 . All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
